f83a54cafcbd3eb8a60603f0b76755158e649a4a
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2023-11-23 17:37:18 +0100

    setup3412

--- prva/lib/main.dart
+++ prva/lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget{
-
-  Widget build(BuildContext context){
-
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter login demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new LoginPage()
-    );
+        title: 'Flutter login demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new LoginPage());
   }
 }
