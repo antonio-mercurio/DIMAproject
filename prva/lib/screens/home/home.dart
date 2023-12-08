483521fd203742a4aa68334d381aea1274b33e7b
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2023-12-08 19:12:52 +0100

    SignOut

--- prva/lib/screens/home/home.dart
+++ prva/lib/screens/home/home.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:prva/services/auth.dart';
 
 class Home extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
+    return Scaffold(
+      backgroundColor: Colors.orange[50],
+      appBar: AppBar(
+        title: Text('Affinder'),
+        backgroundColor: Colors.orange[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('logout'),
+            onPressed: () async {
+              await _auth.signOut();
+            },
+          )
+        ],
+      ),
     );
   }
 }
