97852305868d85aeae7b32693b53db6030783c4b
luigiafalasca <luigia.falasca@gmail.com>
2023-12-14 15:58:27 +0100

    regsiterForHouse

---
+++ prva/lib/screens/registerForHouse/registerFormHouse.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterFormHouse extends StatefulWidget {
+  const RegisterFormHouse({super.key});
+
+  @override
+  State<RegisterFormHouse> createState() => _RegisterFormHouseState();
+}
+
+class _RegisterFormHouseState extends State<RegisterFormHouse> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
