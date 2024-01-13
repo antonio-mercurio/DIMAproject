2cf59c6431e732ff13b036ef3c76a933713e490f
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2024-01-13 10:30:06 +0100

    updatePersonalInfo

--- prva/lib/screens/home/home.dart
+++ prva/lib/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:prva/models/house.dart';
 import 'package:prva/screens/home/houses_list.dart';
+import 'package:prva/screens/home/personalProfileForm.dart';
 import 'package:prva/screens/home/settings_form.dart';
 import 'package:prva/screens/registerForHouse/registerFormHouse.dart';
 import 'package:prva/services/auth.dart';
@@ -19,7 +20,7 @@ class Home extends StatelessWidget {
           builder: (context) {
             return Container(
               padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
-              child: SettingsForm(),
+              child: PersonalProfileForm(),
             );
           });
     }
@@ -45,13 +46,11 @@ class Home extends StatelessWidget {
               icon: Icon(Icons.add_circle),
               label: Text('aggiungi profilo'),
               onPressed: () {
-                 Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      RegisterFormHouse()),
-                 );
-                 print("premuto");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => RegisterFormHouse()),
+                );
+                print("premuto");
               },
             ),
             TextButton.icon(
