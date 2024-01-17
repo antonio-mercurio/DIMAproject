938f6971ff4019cb170f81be5df9769ce4694637
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2024-01-17 22:45:28 +0100

    ultimoFixFiltri

--- prva/lib/services/database.dart
+++ prva/lib/services/database.dart
@@ -74,7 +74,7 @@ class DatabaseService {
 */
     if (selectedFilters != null) {
       if (selectedFilters.maxAge != null) {
-        query = query.where('age', isLessThan: selectedFilters.maxAge);
+        query = query.where('age', isLessThanOrEqualTo: selectedFilters.maxAge);
       }
       if (selectedFilters.minAge != null) {
         query = query.where('age', isGreaterThan: selectedFilters.minAge);
