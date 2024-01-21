41fab1aca76cd96a3bbfd6070dcd161ec4d09e35
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2024-01-21 01:39:55 +0100

    riesco a uploadare immagini anche con iphone

--- prva/lib/schermiProva.dart
+++ prva/lib/schermiProva.dart
@@ -17,6 +17,10 @@ class SchermiProva extends StatefulWidget {
 //for multiple images: https://stackoverflow.com/questions/63513002/how-can-i-upload-multiple-images-to-firebase-in-flutter-and-get-all-their-downlo
 class _SchermiProvaState extends State<SchermiProva> {
   String imageUrl = '';
+  /*final metadata = SettableMetadata(customMetadata: {
+    'uploaded_by': 'gnegno',
+    'description': 'Some description...'
+  });*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +39,7 @@ class _SchermiProvaState extends State<SchermiProva> {
     //1: pick image
     ImagePicker imagePicker = ImagePicker();
     XFile? file = await imagePicker.pickImage(source: ImageSource.gallery);
-    print('${file?.path}');
+    //print('${file?.path}');
     if (file == null) {
       return;
     }
@@ -44,23 +48,29 @@ class _SchermiProvaState extends State<SchermiProva> {
     Reference referenceRoot = FirebaseStorage.instance.ref();
     Reference referenceDirImages = referenceRoot.child('images');
     Reference referenceImageToUpload = referenceDirImages.child(uniqueFileName);
-    print('37');
+    //print('37');
 
     try {
-      final File xFile = await File(file!.path).create();
-      final uploadTask = referenceImageToUpload.putFile(xFile);
-      print('50');
-      final snapshot = await uploadTask!.whenComplete(() {});
+      //final File xFile = await File(file!.path).create();
+      final fileBytes = await file.readAsBytes();
+      //final uploadTask = await referenceImageToUpload.putFile(xFile, metadata);
+      final uploadTask = await referenceImageToUpload.putData(fileBytes);
+      //print('50');
+      //final snapshot = await uploadTask.whenComplete(() {});
 
       //referenceImageToUpload.putData(data);
-      print('task iniziato');
+      //print('task iniziato');
 
-      print('4532');
+      //print('4532');
 
+      //USARE QUESTA STRINGA IMAGEURL PER SCARICARE LE IMMAGINI POI --
+      // Salvare imageUrl in profili : i profili avranno un attributo "fotoProfilo" di tipo stringa
+      //String fotoProfilo = imageUrl (tipo così)
+      //quando poi mi serve stampare la foto faccio il widget di tipo Image.network(imageUrl)
       imageUrl = await referenceImageToUpload.getDownloadURL();
 
-      print('45 schermi');
-      print(imageUrl);
+      //print('45 schermi');
+      //print(imageUrl);
     } catch (e) {
       print('exception');
       print(e);
