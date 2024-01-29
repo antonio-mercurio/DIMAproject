e2ec564f24a5227489739db19aa2e76557dc30f7
luigiafalasca <luigia.falasca@gmail.com>
2024-01-29 12:43:31 +0100

    form house 2

--- prva/lib/form_house_profile_adj.dart
+++ prva/lib/form_house_profile_adj.dart
@@ -1,5 +1,10 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:geocoding_resolver/geocoding_resolver.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_places_flutter/google_places_flutter.dart';
+import 'package:google_places_flutter/model/prediction.dart';
 import 'package:intl/intl.dart';
 import 'package:prva/schermiProva.dart';
 
@@ -15,6 +20,22 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
 
   final scaffoldKey = GlobalKey<FormState>();
 
+  TextEditingController controller = TextEditingController();
+  late LatLng selLocation;
+
+  int? _numberBathroom;
+  int? _floorNumber;
+  int? _numberPeople;
+  double? _price;
+  String? _description;
+  String? _type;
+   final List<String> typeOfAppartament = [
+    "Apartment",
+    "Single room",
+    "Double room",
+    "Studio apartment",
+    "Two-room apartment",
+  ];
   DateTime? _startDate;
   DateTime? _endDate;
   List<String> imageURLs =  ['', '', '', ''];
@@ -62,6 +83,8 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
           child: Align(
             alignment: AlignmentDirectional(0, -1),
             child: SingleChildScrollView(
+              child: Form(
+                key: scaffoldKey,
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -125,10 +148,14 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
                                 child: Container(
                                   width: double.infinity,
-                                  child: TextFormField(
-                                    autofocus: true,
-                                    autofillHints: [AutofillHints.email],
-                                    obscureText: false,
+                                  
+                                  child: DropdownButtonFormField(
+                                    style: TextStyle(
+                                        fontFamily: 'Plus Jakarta Sans',
+                                        color: Color(0xFF101213),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w500,
+                                      ),
                                     decoration: InputDecoration(
                                       labelText: 'Type',
                                       labelStyle: TextStyle(
@@ -172,14 +199,15 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                         fillColor: Colors.white,
                                         contentPadding: EdgeInsets.all(24),
                                       ),
-                                      style: TextStyle(
-                                        fontFamily: 'Plus Jakarta Sans',
-                                        color: Color(0xFF101213),
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    //validator: 
-                                  ),
+                    value: _type ?? "Apartment",
+                    items: typeOfAppartament.map((type) {
+                      return DropdownMenuItem(
+                        value: type,
+                        child: Text('${type} '),
+                      );
+                    }).toList(),
+                    onChanged: (val) => setState(() => _type = val)),
+                                  
                                 ),
                               ),
                               Padding(
@@ -187,11 +215,10 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                     EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
                                 child: Container(
                                   width: double.infinity,
-                                  child: TextFormField(
-                                    autofocus: true,
-                                    autofillHints: [AutofillHints.addressCity],
-                                    obscureText: false,
-                                    decoration: InputDecoration(
+                                  child: GooglePlaceAutoCompleteTextField(
+                textEditingController: controller,
+                googleAPIKey: "AIzaSyD8yblyesPc-09bye4ZF9KlO95G6RhhlmA",
+                inputDecoration: InputDecoration(
                                       labelText: 'Address',
                                       labelStyle: TextStyle(
                                           fontFamily: 'Plus Jakarta Sans',
@@ -204,46 +231,79 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                           color: Color(0xFFE0E3E7),
                                           width: 2,
                                         ),
-                                        borderRadius: BorderRadius.circular(40),
                                       ),
                                       focusedBorder: OutlineInputBorder(
                                           borderSide: BorderSide(
                                             color: Color(0xFF4B39EF),
                                             width: 2,
-                                          ),
-                                          borderRadius:
-                                              BorderRadius.circular(40),
+                                          ), 
                                         ),
                                         errorBorder: OutlineInputBorder(
                                           borderSide: BorderSide(
                                             color: Color(0xFFFF5963),
                                             width: 2,
                                           ),
-                                          borderRadius:
-                                              BorderRadius.circular(40),
                                         ),
                                         focusedErrorBorder: OutlineInputBorder(
                                           borderSide: BorderSide(
                                             color: Color(0xFFFF5963),
                                             width: 2,
                                           ),
-                                          borderRadius:
-                                              BorderRadius.circular(40),
                                         ),
                                         filled: true,
                                         fillColor: Colors.white,
                                         contentPadding: EdgeInsets.all(24),
                                       ),
-                                      style: TextStyle(
-                                        fontFamily: 'Plus Jakarta Sans',
-                                        color: Color(0xFF101213),
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    /*validator: _model
-                                        .addressController1Validator
-                                        .asValidator(context),*/
-                                  ),
+                debounceTime: 800, // default 600 ms,
+                countries: ["it", "fr"], // optional by default null is set
+                isLatLngRequired:
+                    true, // if you required coordinates from place detail
+                getPlaceDetailWithLatLng: (Prediction prediction) async {
+                  // this method will return latlng with place detail
+                  print("placeDetails LNG " + prediction.lng.toString());
+                  print("placeDetails LAT " + prediction.lat.toString());
+                  if (prediction.lng != null && prediction.lat != null) {
+                    selLocation = LatLng(
+                        double.parse(prediction.lat.toString()),
+                        double.parse(prediction.lng.toString()));
+                    var address = await GeoCoder().getAddressFromLatLng(
+                        latitude: selLocation.latitude,
+                        longitude: selLocation.longitude);
+                    //qui accedo ad address e prendo i dettagli che mi servono
+
+                    print('${address.addressDetails.city}');
+                  }
+                }, // this callback is called when isLatLngRequired is true
+
+                itemClick: (Prediction prediction) {
+                  controller.text = prediction.description!;
+                  controller.selection = TextSelection.fromPosition(
+                      TextPosition(offset: prediction.description!.length));
+                },
+                // if we want to make custom list item builder
+                itemBuilder: (context, index, Prediction prediction) {
+                  return Container(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        Icon(Icons.location_on),
+                        SizedBox(
+                          width: 7,
+                        ),
+                        Expanded(child: Text("${prediction.description ?? ""}"))
+                      ],
+                    ),
+                  );
+                },
+                // if you want to add seperator between list items
+                seperatedBuilder: Divider(),
+                // want to show close icon
+                isCrossBtnShown: true,
+                // optional container padding
+                //containerHorizontalPadding: 10,
+              )
+                                  
+                                 
                                 ),
                               ),
                             ],
@@ -302,6 +362,7 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                 child: Container(
                                   width: double.infinity,
                                   child: TextFormField(
+                                    maxLines: 3,
                                     autofocus: true,
                                     autofillHints: [AutofillHints.email],
                                     obscureText: false,
@@ -354,9 +415,11 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                         fontSize: 16,
                                         fontWeight: FontWeight.w500,
                                       ),
-                                    /*validator: _model
-                                        .descriptionControllerValidator
-                                        .asValidator(context),*/
+                                     validator: (val) => val!.isEmpty
+                                          ? 'Please enter description'
+                                          : null,
+                                      onChanged: (val) =>
+                                          setState(() => _description = val),
                                   ),
                                 ),
                               ),
@@ -423,9 +486,11 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                         fontWeight: FontWeight.w500,
                                       ),
                                           keyboardType: TextInputType.number,
-                                         /* validator: _model
-                                              .addressController2Validator
-                                              .asValidator(context),*/
+                                          validator: (val) =>
+                      val!.isEmpty ? 'Please enter a number' : null,
+                  onChanged: (val) =>
+                      setState(() => _numberPeople = (int.parse(val)))
+                                         
                                         ),
                                       ),
                                     ),
@@ -496,9 +561,10 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                         fontWeight: FontWeight.w500,
                                       ),
                                           keyboardType: TextInputType.number,
-                                          /*validator: _model
-                                              .addressController3Validator
-                                              .asValidator(context),*/
+                                         validator: (val) =>
+                      val!.isEmpty ? 'Please enter a number' : null,
+                  onChanged: (val) =>
+                      setState(() => _floorNumber = (int.parse(val)))
                                         ),
                                       ),
                                     ),
@@ -568,9 +634,10 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                                         fontWeight: FontWeight.w500,
                                       ),
                                           keyboardType: TextInputType.number,
-                                         /* validator: _model
-                                              .addressController4Validator
-                                              .asValidator(context),*/
+                                         validator: (val) =>
+                      val!.isEmpty ? 'Please enter a number' : null,
+                  onChanged: (val) =>
+                      setState(() => _numberBathroom = (int.parse(val)))
                                         ),
                                       ),
                                     ),
@@ -584,9 +651,6 @@ class _FormHouseAdjState extends State<FormHouseAdj> {
                     ),
                   ),
                 
-                
-                
-                // Generated code for this Container Widget...
 Padding(
   padding: EdgeInsets.all(12),
   child: Container(
@@ -837,8 +901,10 @@ Padding(
                                       ),
                   keyboardType:
                       const TextInputType.numberWithOptions(decimal: true),
-                  //validator: _model.descriptionController3Validator
-                   //   .asValidator(context),
+                  validator: (val) =>
+                      val!.isEmpty ? 'Please enter a price' : null,
+                  onChanged: (val) =>
+                      setState(() => _price = (double.parse(val)))
                 ),
               ),
             ),
@@ -848,8 +914,6 @@ Padding(
     ),
   ),
 ),
-
-
 
  Padding(
                       padding: const EdgeInsets.all(12),
@@ -1173,13 +1237,71 @@ Padding(
                       ),
                     ),
 
-                
-                
+                    Align(
+                      alignment: const AlignmentDirectional(0, 0),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            if (scaffoldKey.currentState!.validate()) {
+                              if(_startDate!.isBefore(_endDate!)){
+                              if ( imageURLs[0] != ''
+                                   /*&& imageURLs[0]!='' && imageURLs[0]!='' && imageURLs[0]!=''*/) {
+                                setState(() {});
+
+                                print(_price);
+                                print(_numberBathroom);
+                               
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                      'Insert the photos!',
+                                    ),
+                                  ),
+                                );
+                                return;
+                              }
+                            }else{
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                      'End date is before start date!',
+                                    ),
+                                  ),
+                                );
+                                return;
+                            }
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(230, 52),
+                            backgroundColor: Colors.black,
+                            elevation: 3.0,
+                            side: const BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
+                            ),
+                          ),
+                          child: const Text(
+                            'Create',
+                            style: TextStyle(
+                              fontFamily: 'Plus Jakarta Sans',
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),           
                 ],
               ),
             ),
           ),
         ),
+        )
     );
   }
 }
