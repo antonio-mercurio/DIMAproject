f0d5e5ae49dc9da6de551ddc324df3b747d29dd4
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2024-01-29 17:09:56 +0100

    filtersFIXED (si spera per sempre)

--- prva/lib/main.dart
+++ prva/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:prva/form_house_profile_adj.dart';
 import 'package:prva/models/user.dart';
+import 'package:prva/screens/wrapper.dart';
 import 'package:prva/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -24,8 +25,9 @@ class MyApp extends StatelessWidget {
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
-        home: FormHouseAdj()//ViewProfile(houseProfile: HouseProfile(type: 'Stanza Singola', city: 'Milano', price: 500, address: 'Via Schipsrelli', owner: '', idHouse: '' )),
-      ),
+          home:
+              Wrapper() //ViewProfile(houseProfile: HouseProfile(type: 'Stanza Singola', city: 'Milano', price: 500, address: 'Via Schipsrelli', owner: '', idHouse: '' )),
+          ),
     );
   }
 }
