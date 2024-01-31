5728fa1ba7bd250ecc69e622d0d37f6502fb0b6c
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2024-01-31 17:14:41 +0100

    popRoutes

--- prva/lib/main.dart
+++ prva/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:prva/form_filter_people_adj.dart';
 import 'package:prva/form_house_profile_adj.dart';
 import 'package:prva/models/user.dart';
+import 'package:prva/screens/house_profile/show_all_my_house_profile.dart';
 import 'package:prva/screens/wrapper.dart';
+import 'package:prva/screens/wrapperCreationProfile.dart';
 import 'package:prva/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -26,7 +28,15 @@ class MyApp extends StatelessWidget {
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
-        home: Wrapper(),
+        initialRoute: '/',
+        routes: {
+          // When navigating to the "/" route, build the FirstScreen widget.
+          '/': (context) => Wrapper(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+          '/homepageUser': (context) => WrapperCreationProfile(),
+          '/homepageHouse': (context) => ShowHomeProfile(),
+        },
+        //home: Wrapper(),
       ),
     );
   }
