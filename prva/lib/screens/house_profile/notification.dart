20461972849a06dbda5b669ed728f31106bd2698
luigiafalasca <luigia.falasca@gmail.com>
2024-03-05 09:16:58 +0100

    notifiche grafica

--- prva/lib/screens/house_profile/notification.dart
+++ prva/lib/screens/house_profile/notification.dart
@@ -29,7 +29,19 @@ class _NotificationLayoutState extends State<NotificationLayout> {
         setState(() {});
       }
     });
-    return Scaffold(body: _buildNotificationList(context, house, idmatches));
+    return Scaffold(
+      backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Notifications',
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 0,
+        ),
+      body: _buildNotificationList(context, house, idmatches));
   }
 }
 
@@ -46,6 +58,13 @@ Widget _buildNotificationList(
       }
       if (snapshot.hasData) {
         return ListView(
+          padding: EdgeInsets.fromLTRB(
+            0,
+            8,
+            0,
+            44,
+          ),
+          scrollDirection: Axis.vertical,
           children: snapshot.data!.docs
               .map<Widget>((doc) => _buildUserListItem(context, doc, house))
               .toList(),
@@ -69,6 +88,102 @@ Widget _buildUserListItem(
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 241, 242, 244),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 3,
+                      color: Color(0x33000000),
+                      offset: Offset(0, 1),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: Color.fromARGB(255, 62, 62, 62),
+                    width: 1,
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 80,
+                        height: 80,
+                        child: Padding(
+                          padding: EdgeInsets.all(2),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(0),
+                            child: Image.network(
+                              'https://source.unsplash.com/random/1280x720?profile&5',
+                              width: 44,
+                              height: 44,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(12, 0, 4, 0),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'New Match!',
+                                maxLines: 1,
+                                style: const TextStyle(
+                                          fontFamily: 'Plus Jakarta Sans',
+                                          color: Color(0xFF101213),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                                child: Text(
+                                  'You have matched with' + data['name'] + " " + data['surname']+ '! Go to the chat to start a conversation! ',
+                                  maxLines: 4,
+                                  style: 
+                                       const TextStyle(
+                                          fontFamily: 'Plus Jakarta Sans',
+                                          color: Color(0xFF101213),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(0, 8, 0, 4),
+                                child: Text(
+                                  '2 hours ago',
+                                  style: const TextStyle(
+                                          fontFamily: 'Plus Jakarta Sans',
+                                          color: Color(0xFF101213),
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          /* Padding(
               padding: EdgeInsets.only(top: 8.0),
               child: Card(
                   margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
@@ -115,8 +230,8 @@ Widget _buildUserListItem(
                         ]),
                       )
                     ],
-                  )));
+                  )));*/
         } else
-          return Text('no');
+          return Text('');
       });
 }
