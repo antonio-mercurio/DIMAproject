58e30949b93997102f0033bacf24c512722326b9
antonio-mercurio <100923523+antonio-mercurio@users.noreply.github.com>
2024-05-15 11:26:51 +0200

    fix accommodation

--- prva/lib/screens/home/home.dart
+++ prva/lib/screens/home/home.dart
@@ -25,119 +25,123 @@ class _HomepageState extends State<Homepage> {
         appBar: AppBar(
           actions: <Widget>[
             TextButton.icon(
-              icon: Icon(Icons.person,
-              color: backgroundColor,
-              size: MediaQuery.sizeOf(context).width<widthSize 
-                  ? MediaQuery.sizeOf(context).height * 0.03
-                  :  MediaQuery.sizeOf(context).height * 0.032,),
+              icon: Icon(
+                Icons.person,
+                color: backgroundColor,
+                size: MediaQuery.sizeOf(context).width < widthSize
+                    ? MediaQuery.sizeOf(context).height * 0.03
+                    : MediaQuery.sizeOf(context).height * 0.032,
+              ),
               label: Text('Logout',
-              style: TextStyle(
-              color: backgroundColor,
-              fontSize: size14(context),)),
+                  style: TextStyle(
+                    color: backgroundColor,
+                    fontSize: size14(context),
+                  )),
               onPressed: () async {
                 await _auth.signOut();
               },
             ),
           ],
-          title: Text('AFFINDER', style: GoogleFonts.rubik(
-                 color: Colors.white,
-                 fontSize: size32(context),
-                 fontWeight: FontWeight.w600,
-                 wordSpacing: 2.0),
-                 textAlign: TextAlign.center,
-                 ),
-                 backgroundColor: mainColor,),
+          title: Text(
+            'AFFINDER',
+            style: GoogleFonts.rubik(
+                color: Colors.white,
+                fontSize: size32(context),
+                fontWeight: FontWeight.w600,
+                wordSpacing: 2.0),
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: mainColor,
+        ),
         body: SafeArea(
           top: true,
           child: MediaQuery.of(context).size.width < widthSize
-          ? Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: MediaQuery.sizeOf(context).height * 0.4,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                ),
-                child:  _choice('assets/cerca_casa.jpg', 'Find an accomodation!', true) 
-              ),
-              Container(
-                height: MediaQuery.sizeOf(context).height * 0.4,
-                decoration: BoxDecoration(
-                  color: backgroundColor,
-                ),
-                child: _choice('assets/cerca_persone.jpg', 'Offer an accomodation!', false)
-              ),
-            ],
-          )
-          :  Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[    
-                 SizedBox(
-                      width: MediaQuery.sizeOf(context).width * 0.49,
-                      height: MediaQuery.sizeOf(context).height*0.90,
-                      child:  _choice('assets/cerca_casa.jpg', 'Find an accomodation!', true) 
-                  ),  
-                  SizedBox(
-                      width: MediaQuery.sizeOf(context).width * 0.49,
-                      height: MediaQuery.sizeOf(context).height*0.90,
-                      child:  _choice('assets/cerca_persone.jpg', 'Offer an accomodation!', false)
-                  ),
-                
-              ]
-
+              ? Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        height: MediaQuery.sizeOf(context).height * 0.4,
+                        decoration: BoxDecoration(
+                          color: backgroundColor,
+                        ),
+                        child: _choice('assets/cerca_casa.jpg',
+                            'Find an accommodation!', true)),
+                    Container(
+                        height: MediaQuery.sizeOf(context).height * 0.4,
+                        decoration: BoxDecoration(
+                          color: backgroundColor,
+                        ),
+                        child: _choice('assets/cerca_persone.jpg',
+                            'Offer an accommodation!', false)),
+                  ],
+                )
+              : Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                      SizedBox(
+                          width: MediaQuery.sizeOf(context).width * 0.49,
+                          height: MediaQuery.sizeOf(context).height * 0.90,
+                          child: _choice('assets/cerca_casa.jpg',
+                              'Find an accommodation!', true)),
+                      SizedBox(
+                          width: MediaQuery.sizeOf(context).width * 0.49,
+                          height: MediaQuery.sizeOf(context).height * 0.90,
+                          child: _choice('assets/cerca_persone.jpg',
+                              'Offer an accommodation!', false)),
+                    ]),
         ),
-      ),
       ),
     );
   }
 
-  Widget _choice(String picture, String phrase, bool firstChoice){
+  Widget _choice(String picture, String phrase, bool firstChoice) {
     return Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
-                  child: InkWell(
-                    splashColor: Colors.transparent,
-                    focusColor: Colors.transparent,
-                    hoverColor: Colors.transparent,
-                    highlightColor: Colors.transparent,
-                    onTap: () async {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => 
-                            firstChoice? const WrapperCreationProfile()
-                            :  const ShowHomeProfile()),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: Image.asset( picture,
+      padding: const EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+      child: InkWell(
+        splashColor: Colors.transparent,
+        focusColor: Colors.transparent,
+        hoverColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onTap: () async {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => firstChoice
+                    ? const WrapperCreationProfile()
+                    : const ShowHomeProfile()),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: Image.asset(
+                picture,
                 height: MediaQuery.sizeOf(context).height * 0.4,
                 width: MediaQuery.sizeOf(context).height * 0.4,
-                        ).image,
-                        ),
-                      ),
-                      child: Align(
-                        alignment:  MediaQuery.of(context).size.width < widthSize
-                        ? const AlignmentDirectional(0, 1)
-                        : const AlignmentDirectional(0, 0.7),
-                        child: Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 16, 0),
-                          child: Text(
-                            phrase,
-                            style: GoogleFonts.rubik(
-                              color: errorColor,
-                              fontSize: size20(context),
-                              fontWeight: FontWeight.bold,
-                              ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
+              ).image,
+            ),
+          ),
+          child: Align(
+            alignment: MediaQuery.of(context).size.width < widthSize
+                ? const AlignmentDirectional(0, 1)
+                : const AlignmentDirectional(0, 0.7),
+            child: Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 16, 0),
+              child: Text(
+                phrase,
+                style: GoogleFonts.rubik(
+                  color: errorColor,
+                  fontSize: size20(context),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
